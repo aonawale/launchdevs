f5218cda703c1f3edc06afaef22c58c4f4a8a1ae
Ahmed Onawale <ahmedonawale@gmail.com>
2025-09-27 19:15:15 +0100

    Setup netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://codecareercoach.com',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
